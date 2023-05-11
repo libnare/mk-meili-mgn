9fd522ebaf20486e0940ee8f07efba68b9987f8b
caipira113 <caipira@libnare.net>
2023-05-11 10:41:09 +0900

    index_uid as const

--- src/meili.rs
+++ src/meili.rs
@@ -4,18 +4,18 @@ use meilisearch_sdk::task_info::TaskInfo;
 use serde_json::json;
 use crate::config;
 
+const INDEX_UID: &str = "notes";
+
 pub async fn reset(client: &Client, url: &str, config: &config::Config) {
     let http_client = reqwest::Client::new();
 
-    let index_uid = "notes";
-
-    if let Ok(task) = client.index(index_uid).delete().await {
+    if let Ok(task) = client.index(INDEX_UID).delete().await {
         println!("Delete: {:?}", task);
     } else {
         println!("Error occurred while deleting index. Skipping...");
     }
 
-    let task = client.create_index(index_uid, Some("id")).await.unwrap();
+    let task = client.create_index(INDEX_UID, Some("id")).await.unwrap();
     println!("Create: {:?}", task);
 
     let settings = meilisearch_sdk::settings::Settings::new().with_searchable_attributes([
@@ -30,18 +30,18 @@ pub async fn reset(client: &Client, url: &str, config: &config::Config) {
         "channelId",
     ]);
 
-    let task: TaskInfo = client.index(index_uid).set_settings(&settings).await.unwrap();
+    let task: TaskInfo = client.index(INDEX_UID).set_settings(&settings).await.unwrap();
     println!("Settings: {:?}", task);
 
     let pagination = PaginationSetting { max_total_hits: 10000 };
 
-    let task: TaskInfo = client.index(index_uid).set_pagination(pagination).await.unwrap();
+    let task: TaskInfo = client.index(INDEX_UID).set_pagination(pagination).await.unwrap();
     println!("Pagination: {:?}", task);
 
     let request = json!({
         "enabled": false
     });
 
-    let typo_tolerances = http_client.patch(&format!("{}/indexes/{}/settings/typo-tolerance", url, index_uid)).json(&request).header("Authorization", format!("Bearer {}", config.meili.apikey)).send().await.unwrap();
+    let typo_tolerances = http_client.patch(&format!("{}/indexes/{}/settings/typo-tolerance", url, INDEX_UID)).json(&request).header("Authorization", format!("Bearer {}", config.meili.apikey)).send().await.unwrap();
     println!("Typo Tolerance: {:?}", typo_tolerances);
 }
