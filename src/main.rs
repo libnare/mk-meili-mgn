afb7937b5e72418cee69c928b01b323859df113b
caipira113 <caipira@libnare.net>
2023-05-13 02:16:06 +0900

    paint text

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod meili;
 use chrono::Utc;
 use std::{error::Error, sync::Mutex};
 use indicatif::{ProgressBar, ProgressStyle};
+use std::io::Write;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 use crate::{
     config::config,
@@ -17,23 +19,43 @@ const INDEX_UID: &str = "notes";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let config = config().unwrap_or_else(|err| {
-        eprintln!("Configuration error: {}", err);
-        std::process::exit(1);
-    });
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    let config = match config() {
+        Ok(config) => config,
+        Err(err) => {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+            writeln!(&mut stdout, "Configuration error: {}", err)?;
+            stdout.reset()?;
+            std::process::exit(1);
+        }
+    };
 
     let db = connect_db().await.unwrap();
 
     let client = connect_meili().await.unwrap();
 
     if config.meili.reset {
-        reset(&client).await.expect("Failed to reset Meilisearch index");
-        println!("Meilisearch index reset.");
+        match reset(&client).await {
+            Ok(_) => {
+                stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+                writeln!(&mut stdout, "Meilisearch index reset.")?;
+                stdout.reset()?;
+            }
+            Err(e) => {
+                stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+                writeln!(&mut stdout, "Failed to reset Meilisearch index: {}", e)?;
+                stdout.reset()?;
+                std::process::exit(1);
+            }
+        }
     }
 
     let data_vec = query_notes(&db).await.unwrap();
     let data_len = data_vec.len();
-    println!("Retrieved {} notes from database.", data_len);
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(0x87, 0xce, 0xeb))))?; // Sky blue
+    writeln!(&mut stdout, "Retrieved {} notes from database.", data_len)?;
+    stdout.reset()?;
 
     let errors = Mutex::new(Vec::new());
     let chunk_size = 19456; // https://stella.place/notes/9eo7ew8sed
@@ -103,15 +125,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let total_skipped = errors.len();
 
     if total_skipped > 0 {
-        println!("{} errors occurred", total_skipped);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+        writeln!(&mut stdout, "{} errors occurred", total_skipped)?;
         let timestamp = Utc::now().timestamp_millis();
         std::fs::write(format!("error-{}.log", timestamp), errors.join("\n")).unwrap();
-        println!("All errors have been output to error-{}.log", timestamp);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+        writeln!(&mut stdout, "All errors have been output to error-{}.log", timestamp)?;
+        stdout.reset()?;
     }
 
-    println!("{} notes have been added", total_added);
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
+    writeln!(&mut stdout, "{} notes have been added", total_added)?;
+    stdout.reset()?;
     if total_skipped > 0 {
-        println!("{} notes were skipped due to errors", total_skipped);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(0x00, 0x8b, 0x8b))))?; // Dark cyan
+        writeln!(&mut stdout, "{} notes were skipped due to errors", total_skipped)?;
+        stdout.reset()?;
     }
 
     Ok(())
