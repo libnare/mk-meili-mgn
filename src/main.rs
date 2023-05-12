937fad9e7236dc6e08b7f11681778d77b0082e0a
caipira113 <caipira@libnare.net>
2023-05-13 01:12:23 +0900

    count correction

--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,47 @@ mod config;
 mod r#struct;
 mod meili;
 
-use std::{error::Error, sync::Mutex};
 use chrono::Utc;
+use std::{error::Error, sync::Mutex};
 
 use crate::{
     config::config,
-    database::connect_db,
+    database::{connect_db, query_notes},
     meili::{connect_meili, get_request_builder, reset, url},
 };
-use crate::database::query_notes;
 
 const INDEX_UID: &str = "notes";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let config = config().unwrap_or_else(|err| {
-        println!("Configuration error: {}", err);
+        eprintln!("Configuration error: {}", err);
         std::process::exit(1);
     });
+
     let db = connect_db().await.unwrap();
+
     let client = connect_meili().await.unwrap();
 
     if config.meili.reset {
-        reset(&client).await.expect("Failed to Meilisearch reset")
-    } else {
-        println!("Skipping reset");
+        reset(&client).await.expect("Failed to reset Meilisearch index");
+        println!("Meilisearch index reset.");
     }
 
     let data_vec = query_notes(&db).await.unwrap();
     let data_len = data_vec.len();
+    println!("Retrieved {} notes from database.", data_len);
 
     let errors = Mutex::new(Vec::new());
-
     let chunk_size = 19456; // https://stella.place/notes/9eo7ew8sed
     let data_chunks = data_vec.chunks(chunk_size);
+    let mut total_added = 0;
 
     for (chunk_index, data_chunk) in data_chunks.enumerate() {
         let json_array = match serde_json::to_string(data_chunk) {
             Ok(json_array) => json_array,
             Err(e) => {
-                errors.lock().unwrap().push(format!("{}: error: {:?}", chunk_index, e));
+                errors.lock().unwrap().push(format!("Error in chunk {}: {}", chunk_index, e));
                 continue;
             }
         };
@@ -53,7 +54,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let data = match serde_json::from_str(&json_array) {
             Ok(data) => data,
             Err(e) => {
-                errors.lock().unwrap().push(format!("{}: error: {:?}", chunk_index, e));
+                errors.lock().unwrap().push(format!("Error in chunk {}: {}", chunk_index, e));
                 continue;
             }
         };
@@ -70,25 +71,34 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let res = match request_builder.send().await {
             Ok(res) => res,
             Err(e) => {
-                errors.lock().unwrap().push(format!("{}: error: {:?}", chunk_index, e));
+                errors.lock().unwrap().push(format!("Error in chunk {}: {}", chunk_index, e));
                 continue;
             }
         };
 
         let res_status = res.status();
         let time = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        println!("Add documents: {}, {}", res_status, time);
+        println!("Added documents from chunk {}: {}, {}", chunk_index, res_status, time);
+
+        if res_status.is_success() {
+            total_added += data_chunk.len();
+        }
     }
 
     let errors = errors.into_inner().unwrap();
-    if !errors.is_empty() {
-        println!("{} errors occurred", errors.len());
+    let total_skipped = errors.len();
+
+    if total_skipped > 0 {
+        println!("{} errors occurred", total_skipped);
         let timestamp = Utc::now().timestamp_millis();
         std::fs::write(format!("error-{}.log", timestamp), errors.join("\n")).unwrap();
         println!("All errors have been output to error-{}.log", timestamp);
     }
 
-    println!("{} notes have been added", data_len - errors.len());
+    println!("{} notes have been added", total_added);
+    if total_skipped > 0 {
+        println!("{} notes were skipped due to errors", total_skipped);
+    }
 
     Ok(())
 }
