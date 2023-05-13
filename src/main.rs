1013239b194a017a8aa3d7dec13194bc2e262e09
caipira113 <caipira@libnare.net>
2023-05-14 04:47:38 +0900

    RichProgress

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ mod meili;
 
 use chrono::Utc;
 use std::{error::Error, sync::Mutex};
-use indicatif::{ProgressBar, ProgressStyle};
 use std::io::Write;
+use kdam::{BarExt, Column, RichProgress, tqdm};
+use kdam::term::Colorizer;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 use crate::{
@@ -62,19 +63,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let data_chunks = data_vec.chunks(chunk_size);
     let mut total_added = 0;
 
-    let pb = ProgressBar::new(data_len as u64);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({percent}%)").unwrap()
-            .progress_chars("#>-"),
+    let mut pb = RichProgress::new(
+        tqdm!(
+            total = (data_len as u64).try_into().unwrap(),
+            unit_divisor = 1024,
+            unit = " Chunk"
+        ),
+        vec![
+            Column::Spinner(
+                "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+                    .chars()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>(),
+                80.0,
+                1.0,
+            ),
+            Column::text("[bold blue]?"),
+            Column::Bar,
+            Column::Percentage(1),
+            Column::text("•"),
+            Column::CountTotal,
+            Column::text("•"),
+            Column::Rate,
+            Column::text("•"),
+            Column::ElapsedTime,
+        ],
     );
 
     let time = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
-    writeln!(&mut stdout, "<-- {}: Starting indexing. -->", time)?;
-    stdout.reset()?;
+    pb.write(&format!("<-- {}: Starting indexing. -->", time).colorize("yellow"));
 
     for (chunk_index, data_chunk) in data_chunks.enumerate() {
+        pb.replace(1, Column::text("[bold blue]processing"));
         let json_array = match serde_json::to_string(data_chunk) {
             Ok(json_array) => json_array,
             Err(e) => {
@@ -82,7 +102,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 continue;
             }
         };
-
 
         let data = match serde_json::from_str(&json_array) {
             Ok(data) => data,
@@ -104,7 +123,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         if res_status.is_success() {
             total_added += data_chunk.len();
-            pb.inc(data_chunk.len() as u64);
+            let new = std::cmp::min(total_added + chunk_size, data_len);
+            pb.update_to(new);
         } else {
             errors.lock().unwrap().push(
                 format!("Error in chunk {}: {}",
@@ -112,33 +132,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 ));
         }
     }
-
-    pb.finish();
+    pb.replace(1, Column::text("[bold blue]done"));
 
     let time = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
-    writeln!(&mut stdout, "<-- {}: Finished indexing. -->", time)?;
-    stdout.reset()?;
+    pb.write(&format!("<-- {}: Finished indexing. -->", time).colorize("yellow"));
 
     let errors = errors.into_inner().unwrap();
     let total_skipped = errors.len();
 
     if total_skipped > 0 {
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
-        writeln!(&mut stdout, "{} errors occurred", total_skipped)?;
+        writeln!(&mut stdout, "\n{} errors occurred", total_skipped)?;
         let timestamp = Utc::now().timestamp_millis();
         std::fs::write(format!("error-{}.log", timestamp), errors.join("\n")).unwrap();
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
-        writeln!(&mut stdout, "All errors have been output to error-{}.log", timestamp)?;
+        writeln!(&mut stdout, "\nAll errors have been output to error-{}.log", timestamp)?;
         stdout.reset()?;
     }
 
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
-    writeln!(&mut stdout, "{} notes have been added", total_added)?;
+    writeln!(&mut stdout, "\n{} notes have been added", total_added)?;
     stdout.reset()?;
     if total_skipped > 0 {
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(0x00, 0x8b, 0x8b))))?; // Dark cyan
-        writeln!(&mut stdout, "{} notes were skipped due to errors", total_skipped)?;
+        writeln!(&mut stdout, "\n{} notes were skipped due to errors", total_skipped)?;
         stdout.reset()?;
     }
 
